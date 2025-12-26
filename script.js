f2f878912fef9035bc67df16132a00284ed94b5c
SATISH KUMAR <satish21february@gmail.com>
2025-12-26 19:31:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Smooth scroll for navbar links
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href'))
+      .scrollIntoView({ behavior: "smooth" });
+  });
+});
+
